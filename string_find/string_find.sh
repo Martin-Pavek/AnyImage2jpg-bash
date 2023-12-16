a58328101ae15613447908b928615567be60ec16
Martin-Pavek <146820572+Martin-Pavek@users.noreply.github.com>
2023-12-16 09:12:12 +0100

    Update string_find.sh

--- string_find/string_find.sh
+++ string_find/string_find.sh
@@ -2,18 +2,19 @@
 
 file="abcd.bmp"
 
-#file="."
+#hledej="."
 # tecku umi najit
-#file="bc"
-#file="xbc"
-file=".bmp"
-#file=".bmpxxx"
+#hledej="bc"
+#hledej="xbc"
+hledej=".bmp"
+#hledej=".bmpxxx"
 
-if [[ $file == *$file* ]]; then
+
+if [[ $file == *$hledej* ]]; then
 # napise jenom jestli obsahuje nebo neobshuje ale pri pripadnem nalezu uz nepise na jaky pozici nasel
-echo "obsahuje retezec "$file
+echo "obsahuje retezec "$hledej
 else
-echo "neobsahuje retezec "$file
+echo "neobsahuje retezec "$hledej
 fi
 
 
